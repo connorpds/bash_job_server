2cf9369bd1a3cfa529b246c421918852de559cab
Connor Selna <connorselna2021@u.northwestern.edu>
2024-11-04 23:00:21 -0600

    expanded toml structure

--- run_jobs.sh
+++ run_jobs.sh
@@ -1,12 +1,20 @@
-
 program_to_run=$1
+run_args_toml=$2
 
+gpgpu_sim_configs=()
+accel_sim_configs=()
+#TODO:  
+function read_configs(){
+
+}
 
 args=()
+workloads()
 function read_args(){
-  run_args_ct=$(tomlq -r '.run_args | length' hellos.toml)
+  run_args_ct=$(tomlq -r '.run_args | length' $run_args_toml)
   for (( i=0; i<run_args_ct; i++ )); do 
-    args+=("$(tomlq -r ".run_args[$i].arg" hellos.toml)")
+    args+=("$(tomlq -r ".run_args[$i].arg" $run_args_toml)")
+    workloads+=("$(tomlq -r ".run_args[$i].workload" $run_args_toml)")
   done
 }
 
